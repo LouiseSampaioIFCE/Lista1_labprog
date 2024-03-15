d83e00e64c258219020a70b29331947817dddb20
Louise <100720125+louisesampaio@users.noreply.github.com>
2024-03-15 16:48:07 -0300

    questao 12

--- q12.c
+++ q12.c
@@ -2,8 +2,24 @@
 
 int main()
 {
+    int first_num, sec_num;
+    int soma, produto, diferenca, quociente, resto;
+    printf("Digite o primeiro numero: ");
+    scanf(" %d", &first_num);
 
-    //printf("Insira o valor atual da cotacao do dollar:\n");
-   //scanf(" %f", &cotacao);
+    printf("Digite o segundo numero: ");
+    scanf(" %d", &sec_num);
+
+    soma = first_num + sec_num;
+    produto = first_num * sec_num;
+    diferenca = first_num - sec_num;
+    quociente = first_num / sec_num;
+    resto = first_num % sec_num;
+
+    printf("A soma: %d\n", soma);
+    printf("O produto: %d\n", produto);
+    printf("A diferenca: %d\n", diferenca);
+    printf("O quociente: %d\n", quociente);
+    printf("O resto: %d\n", resto);
     return 0;
 }
